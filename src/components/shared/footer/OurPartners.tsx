e55e5bac9d703916a001611525ad3076655b0e8c
sabbir1257 <masabbir1257@gmail.com>
2025-05-29 13:11:06 +0600

    change our partners section

--- src/components/shared/footer/OurPartners.tsx
+++ src/components/shared/footer/OurPartners.tsx
@@ -1,26 +1,41 @@
 /* eslint-disable @next/next/no-img-element */
-import Container from "@/components/ui/Container"
+import Container from "@/components/ui/Container";
 
 const OurPartners = () => {
+  const logos = [
+    "/images/Logo_PNG.png",
+    "/images/logo/p-2.png",
+    "/images/logo/p-3.png",
+    "/images/logo/p-4.png",
+    "/images/logo/p-5.png",
+  ];
+
   return (
     <section className="bg-gray-100 py-2">
-    <Container >
-        <div className="flex md:flex-row flex-col gap-10 lg:gap-0  justify-between items-center">
-            <div>
-            <h1 className="title1">OUR</h1>
-            <h1 className="title1">PARTNERS</h1>
-            </div>
-            <div className="flex flex-wrap gap-8">
-                <img src="/images/logo/p-1.png" alt="logo" />
-                <img src="/images/logo/p-2.png" alt="logo" />
-                <img src="/images/logo/p-3.png" alt="logo" />
-                <img src="/images/logo/p-4.png" alt="logo" />
-                <img src="/images/logo/p-5.png" alt="logo" />
-            </div>
+      <Container>
+        <div className="gap-10 lg:gap-0 justify-between items-center">
+          <div className="flex flex-wrap gap-8">
+            {logos.map((logo, i) => (
+              <img
+                key={i}
+                src={logo}
+                alt="logo"
+                className="filter grayscale brightness-75 hover:grayscale-0 hover:brightness-100 transition duration-300 ease-in-out"
+                style={{
+                  maxHeight: "60px",
+                  cursor: "pointer",
+                  backgroundColor: "white",
+                  padding: "6px",
+                  borderRadius: "6px",
+                  boxShadow: "0 1px 3px rgba(0,0,0,0.1)",
+                }}
+              />
+            ))}
+          </div>
         </div>
-    </Container>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default OurPartners
+export default OurPartners;
