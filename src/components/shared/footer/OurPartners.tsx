0d33505af35cd138c4163af80e923b197f3f23b7
sabbir1257 <masabbir1257@gmail.com>
2025-05-31 11:12:18 +0600

    Fix Vercel deployment issue

--- src/components/shared/footer/OurPartners.tsx
+++ src/components/shared/footer/OurPartners.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import Container from '@/components/ui/Container';
 import Image from 'next/image';
 
@@ -20,7 +19,7 @@ const OurPartners = () => {
               key={i}
               src={logo}
               alt={`partner-logo-${i}`}
-              className="w-30 h-20 filter grayscale brightness-75 hover:grayscale-0 hover:brightness-100 transition duration-300 ease-in-out bg-white p-3 rounded-md shadow-md"
+              className="w-30 h-20 filter grayscale brightness-75 hover:grayscale-0   p-3 "
               width={100}
               height={100}
             />
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,8 @@
 import CoreValues from '@/components/pages/about/CoreValues'
 import OurHistories from '@/components/pages/about/OurHistories'
 import WeAreBestSection from '@/components/pages/about/WeAreBestSection'
-// import TotalAnalysis from '@/components/shared/TotalAnalysis'
-// import Container from '@/components/ui/Container'
+import TotalAnalysis from '@/components/shared/TotalAnalysis'
+import Container from '@/components/ui/Container'
 import EveryPageHeroSection from '@/components/ui/EveryPageHeroSection'
 import React from 'react'
 
@@ -12,11 +12,11 @@ const AboutUs = () => {
       <EveryPageHeroSection title='ABOUT US' imageUrl='/images/land3.jpg'/>
       <OurHistories />
       <WeAreBestSection />
-      {/* <Container className='flex justify-center  text-gray-600' >
+      <Container className='flex justify-center  text-gray-600' >
         <div className='mt-80 md:pt-20 lg:mt-0'>
         <TotalAnalysis/>
         </div>
-      </Container> */}
+      </Container>
       <CoreValues />
     </div>
   )
