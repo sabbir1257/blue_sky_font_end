00855add8c46f1a7a2f9aa12c7ac47fb4d5a4bea
sabbir1257 <masabbir1257@gmail.com>
2025-05-31 11:50:24 +0600

    map url push

--- src/components/shared/EmbeddedMap.tsx
+++ src/components/shared/EmbeddedMap.tsx
@@ -5,7 +5,7 @@ const EmbeddedMap = () => {
     <div className="w-full flex justify-center">
       <div className="w-full sm:w-3/4 md:w-2/3 lg:w-full h-[400px] aspect-video relative">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.645959668134!2d90.3510799760799!3d23.795618487037405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c0e900000001%3A0x85b866bce4c4a5f!2sBlue%20Sky%20CIty%20Limited!5e0!3m2!1sen!2sbd!4v1748231471304!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d116843.50613203371!2d90.0865884!3d23.770207!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755ebef35f05bfd%3A0x2db6405916fbd940!2sBlue%20Sky%20City%20Limited!5e0!3m2!1sen!2sbd!4v1748670430749!5m2!1sen!2sbd"
           width="100%"
           height="400"
           loading="lazy"
