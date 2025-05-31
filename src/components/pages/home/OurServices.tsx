66edf6f84227d3435e3eb024b34ccec20c8809c2
sabbir1257 <masabbir1257@gmail.com>
2025-05-31 19:37:36 +0600

    home page title deagine chnage

--- src/components/pages/home/OurServices.tsx
+++ src/components/pages/home/OurServices.tsx
@@ -1,14 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-import Container from "@/components/ui/Container"
-import Separator from "@/components/ui/Separator"
+import Container from '@/components/ui/Container';
+import Separator from '@/components/ui/Separator';
 
 const OurServices = () => {
   return (
     <Container className="my-20">
-    <section className="flex flex-col items-center gap-5">
-        <h1 className="title">Our SERVICES</h1>
+      <section className="flex flex-col items-center gap-5">
+        <div className="text-center mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold text-blue-900">
+            Our SERVICES{' '}
+          </h2>
+          <div className="w-20 h-1 mx-auto mt-4 bg-blue-600 rounded-full"></div>
+        </div>
         <Separator />
-        <p className="paragraph">A tale of a fateful trip that started from this tropic port aboard this tiny ship today stillers</p>
+        <p className="paragraph">
+          A tale of a fateful trip that started from this tropic port aboard
+          this tiny ship today stillers
+        </p>
         {/* service cards  */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
           <OurServiceCard />
@@ -20,26 +28,38 @@ const OurServices = () => {
         </div>
 
         <div>
-          <h1 className="mt-5 text-xl text-center text-gray-600 leading-8">You can also send us an email and we’ll get in touch shortly, or Call us
-          <br/><span className="text-blue-900 underline">info@support.com (or) +1 234 6780 900</span></h1>
+          <h1 className="mt-5 text-xl text-center text-gray-600 leading-8">
+            You can also send us an email and we’ll get in touch shortly, or
+            Call us
+            <br />
+            <span className="text-blue-900 underline">
+              info@support.com (or) +1 234 6780 900
+            </span>
+          </h1>
         </div>
-    </section>
+      </section>
     </Container>
-  )
-}
+  );
+};
 
 const OurServiceCard = () => {
   return (
     <div className="border rounded-md flex items-center hover:shadow-md transition-all duration-200">
-      <img className="w-[120px] rounded-l-md h-[130px]" src="/images/land.jpg" alt="land" />
+      <img
+        className="w-[120px] rounded-l-md h-[130px]"
+        src="/images/land.jpg"
+        alt="land"
+      />
       <div className="px-8 items-start flex flex-col gap-1">
-        <h1 className="text-2xl font-medium text-gray-700">Business Services</h1>
+        <h1 className="text-2xl font-medium text-gray-700">
+          Business Services
+        </h1>
         <p className="mb-3 text-gray-600">The tiny ship today stiller</p>
 
         <button className="uppercase font-medium">Read more {'>'}</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OurServices
+export default OurServices;
--- src/components/pages/home/OurTeam.tsx
+++ src/components/pages/home/OurTeam.tsx
@@ -1,28 +1,33 @@
-
-import Container from '@/components/ui/Container'
-import Separator from '@/components/ui/Separator'
-import React from 'react'
-import TeamsCard from '../teams/TeamsCard'
+import Container from '@/components/ui/Container';
+import Separator from '@/components/ui/Separator';
+import React from 'react';
+import TeamsCard from '../teams/TeamsCard';
 
 const OurTeam = () => {
-    return (
-        <Container className='mb-20'>
-            <section className="flex flex-col items-center gap-5">
-                <h1 className="title">Our TEAM</h1>
-                <Separator />
-                <p className="paragraph">A tale of a fateful trip that started from this tropic port aboard this tiny ship today stillers</p>
-                {/* service cards  */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5 w-full">
-                    <TeamsCard imageUrl='/images/man1.jpg'/>
-                    <TeamsCard imageUrl='/images/man2.jpg'/>
-                    <TeamsCard imageUrl='/images/man3.jpg'/>
-                    <TeamsCard imageUrl='/images/man4.jpg'/>
-                </div>
-            </section>
-        </Container>
-    )
-}
+  return (
+    <Container className="mb-20">
+      <section className="flex flex-col items-center gap-5">
+        <div className="text-center mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold text-blue-900">
+            Our TEAM{' '}
+          </h2>
+          <div className="w-20 h-1 mx-auto mt-4 bg-blue-600 rounded-full"></div>
+        </div>
+        <Separator />
+        <p className="paragraph">
+          A tale of a fateful trip that started from this tropic port aboard
+          this tiny ship today stillers
+        </p>
+        {/* service cards  */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5 w-full">
+          <TeamsCard imageUrl="/images/man1.jpg" />
+          <TeamsCard imageUrl="/images/man2.jpg" />
+          <TeamsCard imageUrl="/images/man3.jpg" />
+          <TeamsCard imageUrl="/images/man4.jpg" />
+        </div>
+      </section>
+    </Container>
+  );
+};
 
-
-
-export default OurTeam
+export default OurTeam;
