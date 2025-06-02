b647b0dd30308527da6602dd39951450b4bc3698
sabbir1257 <masabbir1257@gmail.com>
2025-06-02 12:47:15 +0600

    step section code change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import AboutCompany from '@/components/pages/home/AboutCompany';
 import CallToAction from '@/components/pages/home/CallToAction';
-// import CaseStudies from '@/components/pages/home/CaseStudies';
-// import Consultation from "@/components/pages/home/Consultation";
 import GetQuotes from '@/components/pages/home/GetQuotes';
 import HeroSection from '@/components/pages/home/HeroSection';
-// import MissionVision from '@/components/pages/home/MissionVission';
 import OurBusiness from '@/components/pages/home/OurBusiness';
 import ProductSection from '@/components/pages/home/ProductSection';
 import StepsSection from '@/components/pages/home/StepsSection';
 import TestimonialSlider from '@/components/pages/home/TestimonialSlider';
-// import WhyChooseUs from '@/components/pages/home/WhyChooseUs';
 import FlowUsSection from '@/components/shared/followUs/FollowUs';
 
 export default function Home() {
@@ -22,11 +18,7 @@ export default function Home() {
         <GetQuotes />
         <ProductSection />
         <OurBusiness />
-        {/* <WhyChooseUs /> */}
         <TestimonialSlider />
-        {/* <CaseStudies /> */}
-        {/* mission and vision section show about page  */}
-        {/* <MissionVision /> */}
       </div>
       <div className="text-center my-12">
         <h2 className="text-3xl md:text-4xl font-bold text-blue-900">
@@ -36,7 +28,6 @@ export default function Home() {
       </div>
       <FlowUsSection />
       <CallToAction />
-      {/* <Consultation /> */}
     </div>
   );
 }
